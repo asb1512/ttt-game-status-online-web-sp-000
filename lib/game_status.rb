2f3af25690baf0b3e8dd894cd61b69234db49e5c
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-19 00:53:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,11 +1,13 @@
 require "pry"
 
 # Helper Method
+# ––––––––
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+# ––––––––
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
